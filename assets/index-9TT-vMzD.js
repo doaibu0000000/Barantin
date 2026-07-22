a98d32ec349ad7b2a3355e3ce970bdda8319205a
doaibu0000000 <doa.ibu0000000@gmail.com>
2026-07-22 13:41:42 +0700

    Updates

--- assets/index-9TT-vMzD.js
+++ assets/index-9TT-vMzD.js
@@ -49,7 +49,7 @@
     <div class="flex flex-col md:flex-row items-start gap-6 w-full max-w-[1600px] mx-auto p-4 md:p-8 min-h-screen">
       ${i()}
       
-      <main class="bg-brand-panel border border-white/5 rounded-xl flex-1 p-6 md:p-8 flex flex-col gap-5 shadow-2xl">
+      <main class="w-full bg-brand-panel border border-white/5 rounded-xl flex-1 p-6 md:p-8 flex flex-col gap-5 shadow-2xl">
         ${c()}
       </main>
     </div>
